17b541f3ee5a27b20ef47cdc54c5e43126d43efb
Kaiko <kaiko@leisue.com>
2018-10-04 00:24:44 +0800

    Leisue Website - Final
    Leisue Website V2.0

--- leisue-website/js/_main.js
+++ leisue-website/js/_main.js
@@ -11,21 +11,21 @@ $('document').ready(function(){
 	});
 
 
-	jQuery(function($){
-	    // Cycle plugin
-	    $('.slides').cycle({
-	        fx:     'none',
-	        speed:   1,
-	        timeout: 400
-	    }).cycle("pause");
-	    // Pause & play on hover
-	    $('.slideshow-block').hover(function(){
-	        $(this).find('.slides').addClass('active').cycle('resume');
-	    }, function(){
-	        $(this).find('.slides').removeClass('active').cycle('pause');
-	    });
+	// jQuery(function($){
+	//     // Cycle plugin
+	//     $('.slides').cycle({
+	//         fx:     'none',
+	//         speed:   1,
+	//         timeout: 400
+	//     }).cycle("pause");
+	//     // Pause & play on hover
+	//     $('.slideshow-block').hover(function(){
+	//         $(this).find('.slides').addClass('active').cycle('resume');
+	//     }, function(){
+	//         $(this).find('.slides').removeClass('active').cycle('pause');
+	//     });
 
-	});
+	// });
 
 
 
